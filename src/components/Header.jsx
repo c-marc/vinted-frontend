8f16d3118ef8655917126fad60861dd532889564
Marc C <mcar176@gmail.com>
2023-05-04 10:29:40 +0200

    Add link to logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
+
 import logo from "../assets/img/logo.png";
 
 const Header = () => {
   return (
     <header>
       <div className="container">
-        <img src={logo} alt="Vinted" />
+        <Link to="/">
+          <img src={logo} alt="Vinted" />
+        </Link>
         <input type="search" placeholder="Recherche des articles" />
         <nav>
           <button>S inscrire </button>
